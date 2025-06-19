1e72b3ec55b6e95ebead860536a7936af2254307
粂　明子 <tere55967@gmail.com>
2025-06-19 10:42:19 +0900

    ストレッチ項目の作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,22 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+  before_action :require_goal_setup, if: :user_signed_in?
   allow_browser versions: :modern
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
+  private
+  def require_goal_setup
+    return if controller_name == "goals" && %w[new create edit update].include?(action_name)
+    return if controller_path == "devise/sessions" && action_name == "destroy"
+
+    goal = current_user.goal
+    if goal.blank? || goal.goal.blank? || goal.content.blank?
+      redirect_to new_goal_path
+    end
   end
 end
