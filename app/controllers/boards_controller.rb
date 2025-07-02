f2186638ba0797773524dd0b3f946ee74a1688a1
粂　明子 <tere55967@gmail.com>
2025-07-02 14:25:44 +0900

    デバッグ追記

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -19,28 +19,43 @@ class BoardsController < ApplicationController
 
     @board = current_user.boards.build(board_params)
 
+    # デバッグ用ログ追加
+    Rails.logger.info "=== Board作成デバッグ ==="
+    Rails.logger.info "Board attributes: #{@board.attributes.inspect}"
+    Rails.logger.info "Board methods: #{@board.methods.grep(/goal/).sort}"
+    Rails.logger.info "Current user goal: #{current_user.goal.inspect}"
+
     if current_user.goal.present?
       @board.goal = current_user.goal
-      @board.goal_title     = current_user.goal.goal
-      @board.goal_content   = current_user.goal.content
-      @board.goal_reward    = current_user.goal.reward
-      @board.goal_punishment = current_user.goal.punishment
+      
+      begin
+        @board.goal_title = current_user.goal.goal
+        @board.goal_content = current_user.goal.content
+        @board.goal_reward = current_user.goal.reward
+        @board.goal_punishment = current_user.goal.punishment
+        Rails.logger.info "Goal fields設定完了"
+      rescue => e
+        Rails.logger.error "Goal fields設定エラー: #{e.message}"
+        Rails.logger.error e.backtrace.join("\n")
+        raise e
+      end
     end
 
     if @board.save
       redirect_to boards_path, success: t('boards.flash_message.create_success')
     else
+      Rails.logger.error "Board保存エラー: #{@board.errors.full_messages}"
       flash.now[:danger] = t('boards.flash_message.create_failure')
       render :new, status: :unprocessable_entity
     end
   end
 
   def show
-  @board = Board.find(params[:id])
+    @board = Board.find(params[:id])
   end
 
   def edit
-  @board = current_user.boards.find(params[:id])
+    @board = current_user.boards.find(params[:id])
   end
 
   def update
@@ -54,9 +69,9 @@ class BoardsController < ApplicationController
   end
 
   def destroy
-  board = current_user.boards.find(params[:id])
-  board.destroy!
-  redirect_to boards_path, success: t('boards.flash_message.destroy_success'), status: :see_other
+    board = current_user.boards.find(params[:id])
+    board.destroy!
+    redirect_to boards_path, success: t('boards.flash_message.destroy_success'), status: :see_other
   end
 
   private
