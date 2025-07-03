5f0e6ea29fb6b617d8c1eda98eda56cf79b26b99
粂　明子 <tere55967@gmail.com>
2025-07-03 10:48:46 +0900

    データベースリセットコード削除

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
+# exit on error
 set -o errexit
 
 bundle install
-DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:reset
-bundle exec rails assets:precompile
-bundle exec rails assets:clean
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
