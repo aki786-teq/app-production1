6bdbf6dea04dc47de7d9490d591fbe4dfad8c070
粂　明子 <tere55967@gmail.com>
2025-08-13 19:43:07 +0900

    検証ツーino-ルでスマホ画面が見れるように修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :require_goal_setup, if: :user_signed_in?
-  allow_browser versions: :modern
+  # allow_browser versions: :modern
   add_flash_types :success, :danger
 
   protected
