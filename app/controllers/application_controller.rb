955b236b1eb59d0e6134fceba83d79e87023128f
粂　明子 <tere55967@gmail.com>
2025-08-22 17:36:25 +0900

    RSpecエラー修正4

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   include Pagy::Backend
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :require_goal_setup, if: :user_signed_in?
+  skip_before_action :require_goal_setup, if: :devise_controller?
   # allow_browser versions: :modern
   add_flash_types :success, :danger
 
@@ -21,21 +22,13 @@ class ApplicationController < ActionController::Base
     root_path
   end
 
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [ :name ])
-  end
-
   private
-  def require_goal_setup
-  # Deviseのログアウト時は許可
-  return if controller_path == "devise/sessions" && action_name == "destroy"
 
-  # goal#newまたはcreateアクションの場合は許可
-  if controller_name == "goals" && %w[new create].include?(action_name)
-    return
-  end
+  def require_goal_setup
+    # goal#newまたはcreateアクションの場合は許可
+    if controller_name == "goals" && %w[new create].include?(action_name)
+      return
+    end
 
     goal = current_user.goal
     # 目標が存在しない、または goal/contentが未入力なら、編集ページ含めすべて制限
