6df8bfebca177f0de5dc20dc81ecef4b44f41e68
粂　明子 <tere55967@gmail.com>
2025-08-23 12:50:06 +0900

    マイグレーションファイル修正

--- db/migrate/20250823000711_remove_unused_columns.rb
+++ db/migrate/20250823000711_remove_unused_columns.rb
@@ -1,10 +1,10 @@
 class RemoveUnusedColumns < ActiveRecord::Migration[7.2]
   def change
     # 物理削除を使用するため、is_deletedカラムは不要
-    remove_column :users, :is_deleted, :boolean
-    remove_column :boards, :is_deleted, :boolean
+    remove_column :users, :is_deleted, :boolean if column_exists?(:users, :is_deleted)
+    remove_column :boards, :is_deleted, :boolean if column_exists?(:boards, :is_deleted)
 
     # start_timeカラムは使用されていないため削除
-    remove_column :boards, :start_time, :datetime
+    remove_column :boards, :start_time, :datetime if column_exists?(:boards, :start_time)
   end
 end
