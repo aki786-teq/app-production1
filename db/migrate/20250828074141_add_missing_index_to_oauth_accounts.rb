5788ac6c8fa68fa7d21b3bede1212920c0dcba1f
粂　明子 <tere55967@gmail.com>
2025-08-29 14:08:13 +0900

    RuboCopコード整形

--- db/migrate/20250828074141_add_missing_index_to_oauth_accounts.rb
+++ db/migrate/20250828074141_add_missing_index_to_oauth_accounts.rb
@@ -1,8 +1,8 @@
 class AddMissingIndexToOauthAccounts < ActiveRecord::Migration[7.2]
   def change
     # インデックスが既に存在するかチェックしてから追加
-    unless index_exists?(:oauth_accounts, [:user_id, :provider], name: "index_oauth_accounts_on_user_id_and_provider")
-      add_index :oauth_accounts, [:user_id, :provider], unique: true, name: "index_oauth_accounts_on_user_id_and_provider"
+    unless index_exists?(:oauth_accounts, [ :user_id, :provider ], name: "index_oauth_accounts_on_user_id_and_provider")
+      add_index :oauth_accounts, [ :user_id, :provider ], unique: true, name: "index_oauth_accounts_on_user_id_and_provider"
     end
   end
 end
