f910af0ccf70dbf688ca7ee226326b225321db37
粂　明子 <tere55967@gmail.com>
2025-10-24 12:00:53 +0900

    N+1問題解消

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,12 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @pagy, @boards = pagy(@user.boards.order(created_at: :desc))
+    @pagy, @boards = pagy(
+      @user.boards
+            .with_attached_image
+            .includes(:cheers, :bookmarks)
+            .order(created_at: :desc)
+    )
     @calendar_posts = @user.boards.order(:created_at)
     @calendar = SimpleCalendar::Calendar.new(params[:month])
     @streak_days = calculate_streak_days(@user)
